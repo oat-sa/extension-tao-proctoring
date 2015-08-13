a912ea823dd1e828c987294c56f7dbfc2206b030
Joel Bout <joel@taotesting.com>
2015-08-13 14:19:31 +0200

    added Interface draft

---
+++ model/ProctorAssignment.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA;
+ *
+ *
+ */
+namespace oat\taoProctoring\model;
+
+use oat\oatbox\user\User;
+use oat\taoFrontOffice\model\Delivery;
+/**
+ * Interface for the proctoringservice
+ * 
+ * @author Joel Bout <joel@taotesting.com>
+ */
+interface ProctorAssignment
+{
+    /**
+     * Get the deliveries accessible by a proctor
+     *
+     * @param User $proctor
+     * @return Delivery[]
+     */
+    public function getProctorableDeliveries(User $proctor);
+
+    /**
+     * Get a delivery
+     * 
+     * @param string $deliveryId
+     * @return Delivery
+     */
+    public function getDelivery($deliveryId);
+
+    /**
+     * Gets the test takers assigned to a delivery
+     *
+     * @param $deliveryId
+     * @param array $options
+     * @return User[]
+     */
+    public function getDeliveryTestTakers($deliveryId, $options = array());
+
+    /**
+     * Gets the test takers available for a delivery
+     *
+     * @param User $proctor
+     * @param string $deliveryId
+     * @param array $options
+     * @return User[]
+     */
+    public function getAvailableTestTakers(User $proctor, $deliveryId, $options = array());
+
+    /**
+     * Assign a test taker to a delivery
+     *
+     * @param string $testTakerId
+     * @param string $deliveryId
+     * @return bool
+     */
+    public function assignTestTaker($testTakerId, $deliveryId);
+
+    /**
+     * Unassign (remove) a test taker to a delivery
+     *
+     * @param string $testTakerId
+     * @param string $deliveryId
+     * @return bool
+     */
+    public function unassignTestTaker($testTakerId, $deliveryId);
+
+    
+    /**
+     * Authorise a test taker to run a delivery
+     *
+     * @param $testTakerId
+     * @param $deliveryId
+     * @return bool
+     */
+    public function authoriseTestTaker($testTakerId, $deliveryId);
+}
---
+++ model/ProctorAuthorisation.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA;
+ *
+ *
+ */
+namespace oat\taoProctoring\model;
+
+use oat\oatbox\user\User;
+use taoDelivery_models_classes_execution_DeliveryExecution;
+/**
+ * Interface for the proctor authorisation
+ * 
+ * @author Joel Bout <joel@taotesting.com>
+ */
+interface ProctorAuthorisation
+{
+    /**
+     * 
+     * @param string $deliveryId
+     * @return taoDelivery_models_classes_execution_DeliveryExecution
+     */
+    public function getPendingDeliveryExecutions($deliveryId);
+    
+    /**
+     * Authorise a delivery execution of 
+     * test taker to run a delivery
+     *
+     * @param string $deliveryExecutionId
+     * @return bool
+     */
+    public function authoriseDeliveryExecution($deliveryExecutionId);
+}
