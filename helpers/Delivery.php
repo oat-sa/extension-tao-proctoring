b68d6fac70de0e719be53f0deef0b7a70924cb10
jsconan <jsconan@gmail.com>
2015-11-13 09:59:59 +0100

    list current executions of a delivery

--- helpers/Delivery.php
+++ helpers/Delivery.php
@@ -25,6 +25,7 @@ use oat\oatbox\service\ServiceManager;
 use \core_kernel_classes_Resource;
 use \common_session_SessionManager;
 use oat\taoProctoring\model\mock\WebServiceMock;
+use oat\taoDelivery\models\classes\execution\DeliveryExecution;
 
 /**
  * This temporary helpers is a temporary way to return data to the controller.
@@ -144,7 +145,138 @@ class Delivery extends Proctoring
     }
 
     /**
-     * Get the agregated data for a filtered set of delivery executions of a given delivery
+     * Gets the aggregated data for a filtered set of delivery executions of a given delivery
+     * This is performance critical, would need to find a way to optimize to obtain such information
+     *
+     * @param $deliveryId
+     * @param array $options
+     * @return array
+     * @throws \Exception
+     * @throws \oat\oatbox\service\ServiceNotFoundException
+     */
+    public static function getCurrentDeliveryExecutions($deliveryId, $options = array()) {
+        if (is_object($deliveryId)) {
+            $delivery = self::getDelivery($deliveryId);
+
+            if (!$delivery) {
+                throw new \Exception('Unknown delivery!');
+            }
+        } else {
+            $delivery = $deliveryId;
+            $deliveryId = $delivery->getUri();
+        }
+
+        $deliveryService = ServiceManager::getServiceManager()->get('taoProctoring/delivery');
+        $deliveryExecutions = $deliveryService->getCurrentDeliveryExecutions($deliveryId, $options);
+        $testTakers = self::collectionToMap($deliveryService->getDeliveryTestTakers($deliveryId, $options));
+
+        $page = self::paginate($deliveryExecutions, $options);
+
+        /**** to be replaced by real stuff ****/
+        // Seeds the random number generator with a fixed value to avoid changes on refresh
+        srand(count($deliveryExecutions));
+        // Sets some mock data for delivery states
+        $mocks = array(
+            array(
+                //client will infer possible action based on the current status
+                'section' => array(
+                    'label' => 'section B',
+                    'position' => 2,
+                    'total' => 3
+                ),
+                'item' => array(
+                    'label' => 'question X',
+                    'position' => 1,
+                    'total' => 9,
+                    'time' => array(
+                        //time unit in second, does not require microsecond precision for human monitoring
+                        'elapsed' => 340,
+                        'total' => 600
+                    )
+                )
+            ),
+            array(
+                'section' => array(
+                    'label' => 'section A',
+                    'position' => 1,
+                    'total' => 3
+                ),
+                'item' => array(
+                    'label' => 'question Y',
+                    'position' => 5,
+                    'total' => 8,
+                    'time' => array(
+                        'elapsed' => 60,
+                        'total' => 600
+                    )
+                )
+            ),
+            array(
+                'section' => array(
+                    'label' => 'section C',
+                    'position' => 3,
+                    'total' => 3
+                ),
+                'item' => array(
+                    'label' => 'question Z',
+                    'position' => 2,
+                    'total' => 4,
+                    'time' => array(
+                        'elapsed' => 540,
+                        'total' => 600
+                    )
+                )
+            ),
+        );
+        /********/
+
+        $executions = array();
+        foreach($page['data'] as $deliveryExecution) {
+            /* @var $deliveryExecution DeliveryExecution */
+            $userId = $deliveryExecution->getUserIdentifier();
+            $executionState = $deliveryExecution->getState();
+            $status = $executionState->getLabel();
+            $state = array(
+                'status' => $status,
+            );
+            $testTaker = array();
+
+            /**** to be replaced by real stuff ****/
+            $state = array_merge($state, WebServiceMock::random($mocks));
+            /********/
+
+            $user = isset($testTakers[$userId]) ? $testTakers[$userId] : null;
+            if ($user) {
+                /* @var $user User */
+                $firstName = self::getUserStringProp($user, PROPERTY_USER_FIRSTNAME);
+                $lastName = self::getUserStringProp($user, PROPERTY_USER_LASTNAME);
+
+                if (empty($firstName) && empty($lastName)) {
+                    $firstName = self::getUserStringProp($user, RDFS_LABEL);
+                }
+
+                $testTaker['id'] = $user->getIdentifier();
+                $testTaker['firstName'] = $firstName;
+                $testTaker['lastName'] = $lastName;
+            }
+
+            $executions[] = array(
+                'id' => $deliveryExecution->getIdentifier(),
+                'delivery' => array(
+                    'label' => $deliveryExecution->getLabel(),
+                ),
+                'testTaker' => $testTaker,
+                'state' => $state,
+            );
+        }
+
+        $page['data'] = $executions;
+
+        return $page;
+    }
+
+    /**
+     * Get the aggregated data for a filtered set of delivery executions of a given delivery
      * This is performance critical, would need to find a way to optimize to obtain such information
      *
      * @param string $deliveryId
