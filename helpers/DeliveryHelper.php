ed02a7e710ce25ead54a8128ffa1ae6fc4539444
jsconan <jsconan@gmail.com>
2015-12-09 16:46:38 +0100

    DeliveryHelper: paginate before detailing

--- helpers/DeliveryHelper.php
+++ helpers/DeliveryHelper.php
@@ -149,24 +149,8 @@ class DeliveryHelper
      * @throws \oat\oatbox\service\ServiceNotFoundException
      */
     public static function getCurrentDeliveryExecutions($deliveryId, $options = array()) {
-        if (is_object($deliveryId)) {
-            $delivery = self::getDelivery($deliveryId);
-
-            if (!$delivery) {
-                throw new \Exception('Unknown delivery!');
-            }
-        } else {
-            $delivery = $deliveryId;
-            $deliveryId = $delivery->getUri();
-        }
-
         $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
-        $deliveryExecutions = $deliveryService->getCurrentDeliveryExecutions($deliveryId, $options);
-
-        $page = DataTableHelper::paginate($deliveryExecutions, $options);
-        $page['data'] = self::adjustDeliveryExecutions($delivery, $page['data']);
-
-        return $page;
+        return self::adjustDeliveryExecutions($deliveryService->getCurrentDeliveryExecutions($deliveryId, $options), $options);
     }
 
     /**
@@ -183,19 +167,14 @@ class DeliveryHelper
         $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
         $deliveries = $deliveryService->getTestCenterDeliveries($testCenter);
 
-        if (count($deliveries)) {
-            $all = array();
-            foreach($deliveries as $delivery) {
-                $deliveryExecutions = $deliveryService->getCurrentDeliveryExecutions($delivery->getUri(), $options);
-                $all = array_merge($all, self::adjustDeliveryExecutions($delivery, $deliveryExecutions));
+        $all = array();
+        foreach($deliveries as $delivery) {
+            if ($delivery->exists()) {
+                $all = array_merge($all, $deliveryService->getCurrentDeliveryExecutions($delivery->getUri(), $options));
             }
-
-            usort($all, array('\\oat\\taoProctoring\\helpers\\DeliveryHelper', 'sortExecutionsByState'));
-
-            return DataTableHelper::paginate($all, $options);
-        } else {
-            return DataTableHelper::paginate(array(), $options);
         }
+
+        return self::adjustDeliveryExecutions($all, $options);
     }
 
     /**
@@ -210,84 +189,36 @@ class DeliveryHelper
      */
     public static function getDeliveryTestTakers($deliveryId, $options = array())
     {
-        if (is_object($deliveryId)) {
-            $delivery = self::getDelivery($deliveryId);
-
-            if (!$delivery) {
-                throw new \Exception('Unknown delivery!');
-            }
-        } else {
-            $delivery = $deliveryId;
-            $deliveryId = $delivery->getUri();
-        }
-
         $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
         $users = $deliveryService->getDeliveryTestTakers($deliveryId, $options);
-        $deliveryExecutions = $deliveryService->getCurrentDeliveryExecutions($deliveryId, $options);
-        $usersStatus = array();
-        foreach($deliveryExecutions as $deliveryExecution) {
-            $userId = $deliveryExecution->getUserIdentifier();
-            $status = $deliveryExecution->getState()->getLabel();
-            $usersStatus[$userId][] = $status;
-        }
+        $delivery = self::getDelivery($deliveryId);
 
-        $page = DataTableHelper::paginate($users, $options);
+        return DataTableHelper::paginate($users, $options, function($users) use ($delivery) {
+            $executionService = \taoDelivery_models_classes_execution_ServiceProxy::singleton();
 
-        $testTakers = array();
-        foreach($page['data'] as $user) {
-            /* @var $user User */
-            $lastName = UserHelper::getUserLastName($user);
-            $firstName = UserHelper::getUserFirstName($user, empty($lastName));
+            $testTakers = array();
+            foreach($users as $user) {
+                /* @var $user User */
+                $userId = $user->getIdentifier();
+                $lastName = UserHelper::getUserLastName($user);
+                $firstName = UserHelper::getUserFirstName($user, empty($lastName));
 
-            $status = '';
-            if (isset($usersStatus[$user->getIdentifier()])) {
-                $status = implode(', ', array_unique($usersStatus[$user->getIdentifier()]));
-            }
-
-            $testTakers[] = array(
-                'id' => $user->getIdentifier(),
-                'firstname' => $firstName,
-                'lastname' => $lastName,
-                'identifier' => $user->getIdentifier(),
-                'status' => $status,
-            );
-        }
-
-        $page['data'] = $testTakers;
-
-        return $page;
-    }
-
-    /**
-     * Gets the list of test takers assigned to all deliveries
-     *
-     * @param $testCenter
-     * @param array [$options]
-     * @return array
-     */
-    public static function getAllDeliveryTestTakers($testCenter, $options = array()){
-        $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
-        $deliveries = $deliveryService->getTestCenterDeliveries($testCenter);
-
-        if (count($deliveries)) {
-            $all = array();
-            foreach($deliveries as $delivery) {
-                $testTakers = self::getDeliveryTestTakers($delivery);
-                if (isset($testTakers['data'])) {
-                    foreach($testTakers['data'] as $testTaker) {
-                        $testTaker['delivery'] = array(
-                            'uri' => $delivery->getUri(),
-                            'label' => $delivery->getLabel(),
-                        );
-                        $all[] = $testTaker;
-                    }
+                $status = array();
+                $executions = $executionService->getUserExecutions($delivery, $userId);
+                foreach ($executions as $execution) {
+                    $status[] = $execution->getState()->getLabel();
                 }
-            }
 
-            return DataTableHelper::paginate($all, $options);
-        } else {
-            return DataTableHelper::paginate(array(), $options);
-        }
+                $testTakers[] = array(
+                    'id' => $userId,
+                    'firstname' => $firstName,
+                    'lastname' => $lastName,
+                    'identifier' => $userId,
+                    'status' => implode(', ', array_unique($status)),
+                );
+            }
+            return $testTakers;
+        });
     }
 
     /**
@@ -306,25 +237,23 @@ class DeliveryHelper
         $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
         $users = $deliveryService->getAvailableTestTakers($currentUser, $deliveryId, $options);
 
-        $page = DataTableHelper::paginate($users, $options);
+        return DataTableHelper::paginate($users, $options, function($users) {
+            $testTakers = array();
+            foreach($users as $user) {
+                /* @var $user User */
+                $userId = $user->getIdentifier();
+                $lastName = UserHelper::getUserLastName($user);
+                $firstName = UserHelper::getUserFirstName($user, empty($lastName));
 
-        $testTakers = array();
-        foreach($page['data'] as $user) {
-            /* @var $user User */
-            $lastName = UserHelper::getUserLastName($user);
-            $firstName = UserHelper::getUserFirstName($user, empty($lastName));
-
-            $testTakers[] = array(
-                'id' => $user->getIdentifier(),
-                'firstname' => $firstName,
-                'lastname' => $lastName,
-                'identifier' => $user->getIdentifier(),
-            );
-        }
-
-        $page['data'] = $testTakers;
-
-        return $page;
+                $testTakers[] = array(
+                    'id' => $userId,
+                    'firstname' => $firstName,
+                    'lastname' => $lastName,
+                    'identifier' => $userId,
+                );
+            }
+            return $testTakers;
+        });
     }
 
     /**
@@ -462,157 +391,116 @@ class DeliveryHelper
     }
 
     /**
-     * Sort delivery executions by state.
-     * Not to be used on DeliveryExecution instances. This is only designed to sort adjusted array of executions.
-     *
-     * @param array $a
-     * @param array $b
-     * @return int
-     */
-    public static function sortExecutionsByState($a, $b)
-    {
-        $allowedStates = DeliveryHelper::getAllowedStates();
-        $aState = isset($allowedStates[$a['state']['status']]) ? $allowedStates[$a['state']['status']] : 0;
-        $bState = isset($allowedStates[$b['state']['status']]) ? $allowedStates[$b['state']['status']] : 0;
-        $result = $aState - $bState;
-
-        if (!$result) {
-            $result = strcmp($a['date'], $b['date']);
-        }
-
-        if (!$result) {
-            $result = strcmp($a['delivery']['label'], $b['delivery']['label']);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Hash table for allowed states
-     * @var array
-     */
-    private static $allowedStates;
-
-    /**
-     * Gets a hash table to translate allowed states in state numerical identifier (position for sorting)
-     * @return array
-     * @throws \oat\oatbox\service\ServiceNotFoundException
-     */
-    private static function getAllowedStates()
-    {
-        if (!self::$allowedStates) {
-            $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
-            self::$allowedStates = array_flip($deliveryService->getAllowedStates());
-        }
-        return self::$allowedStates;
-    }
-
-    /**
      * Adjusts a list of delivery executions: add information, format the result
      *
-     * @param core_kernel_classes_Resource$delivery
      * @param DeliveryExecution[] $deliveryExecutions
+     * @param array $options
      * @return array
      * @throws \oat\oatbox\service\ServiceNotFoundException
      */
-    private static function adjustDeliveryExecutions($delivery, $deliveryExecutions) {
-        $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
-        $testTakers = array();
-        foreach($deliveryService->getDeliveryTestTakers($delivery->getUri()) as $resource) {
-            $testTakers[$resource->getIdentifier()] = $resource;
-        }
+    private static function adjustDeliveryExecutions($deliveryExecutions, $options) {
+        // sort all executions by reverse date
+        usort($deliveryExecutions, function($a, $b) {
+            return -strcmp(DateHelper::getTimeStamp($a->getStartTime()), DateHelper::getTimeStamp($b->getStartTime()));
+        });
 
-        /**** to be replaced by real stuff ****/
-        // Seeds the random number generator with a fixed value to avoid changes on refresh
-        srand(count($deliveryExecutions));
-        // Sets some mock data for delivery states
-        $mocks = array(
-            array(
-                //client will infer possible action based on the current status
-                'section' => array(
-                    'label' => 'section B',
-                    'position' => 2,
-                    'total' => 3
-                ),
-                'item' => array(
-                    'label' => 'question X',
-                    'position' => 1,
-                    'total' => 9,
-                    'time' => array(
-                        //time unit in second, does not require microsecond precision for human monitoring
-                        'elapsed' => 340,
-                        'total' => 600
-                    )
-                )
-            ),
-            array(
-                'section' => array(
-                    'label' => 'section A',
-                    'position' => 1,
-                    'total' => 3
-                ),
-                'item' => array(
-                    'label' => 'question Y',
-                    'position' => 5,
-                    'total' => 8,
-                    'time' => array(
-                        'elapsed' => 60,
-                        'total' => 600
-                    )
-                )
-            ),
-            array(
-                'section' => array(
-                    'label' => 'section C',
-                    'position' => 3,
-                    'total' => 3
-                ),
-                'item' => array(
-                    'label' => 'question Z',
-                    'position' => 2,
-                    'total' => 4,
-                    'time' => array(
-                        'elapsed' => 540,
-                        'total' => 600
-                    )
-                )
-            ),
-        );
-        /********/
-
-        $executions = array();
-        foreach($deliveryExecutions as $deliveryExecution) {
-
-            $userId = $deliveryExecution->getUserIdentifier();
-            $state = array(
-                'status' => $deliveryService->getState($deliveryExecution),
-            );
-            $testTaker = array();
+        // paginate, then format the data
+        return DataTableHelper::paginate($deliveryExecutions, $options, function($deliveryExecutions) {
+            $deliveryService = ServiceManager::getServiceManager()->get(DeliveryService::CONFIG_ID);
 
             /**** to be replaced by real stuff ****/
-            $state = array_merge($state, WebServiceMock::random($mocks));
+            // Seeds the random number generator with a fixed value to avoid changes on refresh
+            srand(count($deliveryExecutions));
+            // Sets some mock data for delivery states
+            $mocks = array(
+                array(
+                    //client will infer possible action based on the current status
+                    'section' => array(
+                        'label' => 'section B',
+                        'position' => 2,
+                        'total' => 3
+                    ),
+                    'item' => array(
+                        'label' => 'question X',
+                        'position' => 1,
+                        'total' => 9,
+                        'time' => array(
+                            //time unit in second, does not require microsecond precision for human monitoring
+                            'elapsed' => 340,
+                            'total' => 600
+                        )
+                    )
+                ),
+                array(
+                    'section' => array(
+                        'label' => 'section A',
+                        'position' => 1,
+                        'total' => 3
+                    ),
+                    'item' => array(
+                        'label' => 'question Y',
+                        'position' => 5,
+                        'total' => 8,
+                        'time' => array(
+                            'elapsed' => 60,
+                            'total' => 600
+                        )
+                    )
+                ),
+                array(
+                    'section' => array(
+                        'label' => 'section C',
+                        'position' => 3,
+                        'total' => 3
+                    ),
+                    'item' => array(
+                        'label' => 'question Z',
+                        'position' => 2,
+                        'total' => 4,
+                        'time' => array(
+                            'elapsed' => 540,
+                            'total' => 600
+                        )
+                    )
+                ),
+            );
             /********/
 
-            $user = isset($testTakers[$userId]) ? $testTakers[$userId] : null;
-            if ($user) {
-                /* @var $user User */
-                $testTaker['id'] = $user->getIdentifier();
-                $testTaker['lastName'] = UserHelper::getUserLastName($user);
-                $testTaker['firstName'] = UserHelper::getUserFirstName($user, empty($testTaker['lastName']));
+            $executions = array();
+            foreach($deliveryExecutions as $deliveryExecution) {
+
+                $userId = $deliveryExecution->getUserIdentifier();
+                $state = array(
+                    'status' => $deliveryService->getState($deliveryExecution),
+                );
+                $testTaker = array();
+
+                /**** to be replaced by real stuff ****/
+                $state = array_merge($state, WebServiceMock::random($mocks));
+                /********/
+
+                $user = UserHelper::getUser($userId);
+                if ($user) {
+                    /* @var $user User */
+                    $testTaker['id'] = $user->getIdentifier();
+                    $testTaker['lastName'] = UserHelper::getUserLastName($user);
+                    $testTaker['firstName'] = UserHelper::getUserFirstName($user, empty($testTaker['lastName']));
+                }
+
+                $delivery = $deliveryExecution->getDelivery();
+                $executions[] = array(
+                    'id' => $deliveryExecution->getIdentifier(),
+                    'delivery' => array(
+                        'uri' => $delivery->getUri(),
+                        'label' => $delivery->getLabel(),
+                    ),
+                    'date' => DateHelper::displayeDate($deliveryExecution->getStartTime()),
+                    'testTaker' => $testTaker,
+                    'state' => $state,
+                );
             }
 
-            $executions[] = array(
-                'id' => $deliveryExecution->getIdentifier(),
-                'delivery' => array(
-                    'uri' => $delivery->getUri(),
-                    'label' => $delivery->getLabel(),
-                ),
-                'date' => DateHelper::displayeDate($deliveryExecution->getStartTime()),
-                'testTaker' => $testTaker,
-                'state' => $state,
-            );
-        }
-
-        return $executions;
+            return $executions;
+        });
     }
 }
