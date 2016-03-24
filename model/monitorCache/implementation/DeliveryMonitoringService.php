c08e22dd361828b7e9b7b78cca40bdcea5060381
Joel Bout <joel@taotesting.com>
2016-03-24 11:39:42 +0100

    Returned COLUMN_ID that was removed by mistake

--- model/monitorCache/implementation/DeliveryMonitoringService.php
+++ model/monitorCache/implementation/DeliveryMonitoringService.php
@@ -64,6 +64,7 @@ class DeliveryMonitoringService extends ConfigurableService implements DeliveryM
 
     const TABLE_NAME = 'delivery_monitoring';
 
+    const COLUMN_ID = 'id';
     const COLUMN_DELIVERY_EXECUTION_ID = DeliveryMonitoringServiceInterface::DELIVERY_EXECUTION_ID;
     const COLUMN_STATUS = DeliveryMonitoringServiceInterface::STATUS;
     const COLUMN_CURRENT_ASSESSMENT_ITEM = DeliveryMonitoringServiceInterface::CURRENT_ASSESSMENT_ITEM;
