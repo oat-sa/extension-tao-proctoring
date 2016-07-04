d92f4700485042c7a6a30d595ce50cdce9975ef3
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-07-04 12:25:51 +0300

    increase version of the extension

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
 	'label' => 'Proctoring',
 	'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '1.16.2',
+    'version' => '1.2.0',
 	'author' => 'Open Assessment Technologies SA',
 	'requires' => array(
         'tao' => '>=4.5.0',
