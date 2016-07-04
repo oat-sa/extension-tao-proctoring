6a2b3bd1974afd76cb6da9fa130e76fd9a54a4f4
Lionel Lecaque <lionel@taotesting.com>
2016-07-04 16:22:54 +0000

    bump version

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=4.5.0',
