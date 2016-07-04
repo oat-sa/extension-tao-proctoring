c2dc61653dc8caf938d7d7f1c6fddf1f562b77ed
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-07-04 13:31:46 +0200

    fix casting issue

--- model/EligibilityService.php
+++ model/EligibilityService.php
@@ -306,7 +306,7 @@ class EligibilityService extends tao_models_classes_ClassService
     public function canByPassProctor(Resource $eligibility)
     {
         $canByPass = $eligibility->getOnePropertyValue(new Property(self::PROPERTY_BYPASSPROCTOR_URI));
-        return ($canByPass == true);
+        return ($canByPass->getUri() == self::BOOLEAN_TRUE);
     }
 
     /**
