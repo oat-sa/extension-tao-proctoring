a0ad1339d4b2734d875d89fa62a1d98d6ce6933d
Aleh Hutnikau <hutnikau@1pt.com>
2016-10-27 14:14:53 -0500

    add run and resume event to the delivery log

--- manifest.php
+++ manifest.php
@@ -25,7 +25,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '3.6.15',
+    'version' => '3.6.16',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=4.5.0',
