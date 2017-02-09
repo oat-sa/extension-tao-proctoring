e9d8bd7a07195ab9908104a93af153b70ccb5505
antoinerobin <antoine.robin@vesperiagroup.com>
2017-02-09 16:30:14 +0100

    auto authorize guest users

--- model/authorization/ProctorAuthorizationProvider.php
+++ model/authorization/ProctorAuthorizationProvider.php
@@ -22,6 +22,9 @@ namespace oat\taoProctoring\model\authorization;
 use oat\oatbox\service\ConfigurableService;
 use oat\taoDelivery\model\authorization\AuthorizationProvider;
 use oat\taoDelivery\model\execution\DeliveryExecution;
+use oat\taoDeliveryRdf\model\guest\GuestTestUser;
+use oat\taoProctoring\helpers\DeliveryHelper;
+use oat\taoProctoring\model\DeliveryExecutionStateService;
 use oat\taoProctoring\model\execution\DeliveryExecution as ProctoredDeliveryExecution;
 use oat\taoDelivery\model\authorization\UnAuthorizedException;
 use oat\oatbox\user\User;
@@ -56,9 +59,33 @@ class ProctorAuthorizationProvider extends ConfigurableService implements Author
                 'Terminated/Finished delivery cannot be resumed'
             );
         }
+
+        if($user instanceof GuestTestUser && $this->hasDeliveryGuestAccess($deliveryExecution->getDelivery())){
+            $deliveryExecutionStateService = $this->getServiceManager()->get(DeliveryExecutionStateService::SERVICE_ID);
+            $deliveryExecutionStateService->waitExecution($deliveryExecution);
+            DeliveryHelper::authoriseExecutions([$deliveryExecution]);
+        }
+
         if ($state !== ProctoredDeliveryExecution::STATE_AUTHORIZED) {
             $errorPage = _url('awaitingAuthorization', 'DeliveryServer', 'taoProctoring', array('deliveryExecution' => $deliveryExecution->getIdentifier()));
             throw new UnAuthorizedException($errorPage, 'Proctor authorization missing');
         }
+    }
+
+    protected function hasDeliveryGuestAccess(\core_kernel_classes_Resource $delivery )
+    {
+        $returnValue = false;
+
+        $properties = $delivery->getPropertiesValues(array(
+            new \core_kernel_classes_Property(TAO_DELIVERY_ACCESS_SETTINGS_PROP),
+        ));
+        $propAccessSettings = current($properties[TAO_DELIVERY_ACCESS_SETTINGS_PROP]);
+        $accessSetting = (!(is_object($propAccessSettings)) or ($propAccessSettings=="")) ? null : $propAccessSettings->getUri();
+
+        if( !is_null($accessSetting) ){
+            $returnValue = ($accessSetting === DELIVERY_GUEST_ACCESS);
+        }
+
+        return $returnValue;
     }
 }
