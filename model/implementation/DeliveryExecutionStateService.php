3f43bbd38ee6ab4e54dbbfdc569dec4e7a5f8939
Aleh Hutnikau <hutnikau@1pt.com>
2017-03-02 13:24:39 +0200

    Get timestamp right before event (#294)

--- model/implementation/DeliveryExecutionStateService.php
+++ model/implementation/DeliveryExecutionStateService.php
@@ -80,14 +80,15 @@ class DeliveryExecutionStateService extends ConfigurableService implements \oat\
             'web_browser_version' => WebBrowserService::singleton()->getClientVersion(),
             'os_name' => OSService::singleton()->getClientName(),
             'os_version' => OSService::singleton()->getClientVersion(),
-            'timestamp' => microtime(true),
         ];
 
         if ($session) {
             $session->resume();
             $this->getTestSessionService()->persist($session);
+            $logData['timestamp'] = microtime(true);
             $this->getDeliveryLogService()->log($deliveryExecution->getIdentifier(), 'TEST_RESUME', $logData);
         } else {
+            $logData['timestamp'] = microtime(true);
             $this->getDeliveryLogService()->log($deliveryExecution->getIdentifier(), 'TEST_RUN', $logData);
         }
         $deliveryExecution->setState(ProctoredDeliveryExecution::STATE_ACTIVE);
