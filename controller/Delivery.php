ac4347a983c559069a04921e1cf35d37e439fc89
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-04-14 18:27:27 +0300

    Added autorefresh to the page monitoringAll

--- controller/Delivery.php
+++ controller/Delivery.php
@@ -136,6 +136,8 @@ class Delivery extends ProctoringModule
                 'categories' => $this->getAllReasonsCategories(),
                 'printReportButton' => json_encode($assessmentResultsService->getOption(AssessmentResultsService::OPTION_PRINT_REPORT_BUTTON)),
                 'timeHandling' => json_encode($deliveryExecutionStateService->getOption(DeliveryExecutionStateService::OPTION_TIME_HANDLING)),
+                'refreshBtn' => $this->getServiceManager()->get(GuiSettingsService::SERVICE_ID)->getOption(GuiSettingsService::PROCTORING_REFRESH_BUTTON),
+                'autoRefresh' => $this->getServiceManager()->get(GuiSettingsService::SERVICE_ID)->getOption(GuiSettingsService::PROCTORING_AUTO_REFRESH),
             ),
             array(
                 BreadcrumbsHelper::testCenters(),
