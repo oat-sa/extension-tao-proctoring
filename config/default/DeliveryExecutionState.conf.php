3e35d1d1782e350323803f3410f435a85dc9056c
jsconan <jsconan@gmail.com>
2017-04-24 15:07:42 +0200

    Set default timer accuracy warning threshold to 15min

--- config/default/DeliveryExecutionState.conf.php
+++ config/default/DeliveryExecutionState.conf.php
@@ -7,5 +7,5 @@ return new DeliveryExecutionStateService([
     DeliveryExecutionStateService::OPTION_TERMINATION_DELAY_AFTER_PAUSE => 'PT1H',
     DeliveryExecutionStateService::OPTION_TIME_HANDLING => false,
     DeliveryExecutionStateService::OPTION_APPROXIMATE_TIMER => false,
-    DeliveryExecutionStateService::OPTION_APPROXIMATE_TIMER_WARNING => 1440,
+    DeliveryExecutionStateService::OPTION_APPROXIMATE_TIMER_WARNING => 900,
 ]);
