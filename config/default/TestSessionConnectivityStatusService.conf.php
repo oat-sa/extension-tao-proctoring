4f6cefbb0c7fc6de15ec494c675d054259a2af92
antoinerobin <antoine.robin.2014@utt.fr>
2017-06-13 10:29:08 +0200

    configure false by default

--- config/default/TestSessionConnectivityStatusService.conf.php
+++ config/default/TestSessionConnectivityStatusService.conf.php
@@ -5,4 +5,7 @@
  * To replace this add a file C:\domains\package-tao\taoProctoring\config/header/TestSessionConnectivityStatusService.conf.php
  */
 
-return new oat\taoProctoring\model\implementation\TestSessionConnectivityStatusService();
+return new oat\taoProctoring\model\implementation\TestSessionConnectivityStatusService([
+        \oat\taoProctoring\model\implementation\TestSessionConnectivityStatusService::HAS_ONLINE_MODE => false
+    ]
+);
