211d2eebd2c8b911f260fbe6acbf8f672fe74547
Vilmos "Chilly" Kovacs <vilmos@taotesting.com>
2017-06-28 16:39:41 +0200

    TAO-4575 version increment

--- manifest.php
+++ manifest.php
@@ -44,7 +44,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '5.16.3',
+    'version' => '5.16.4',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=10.24.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -381,6 +381,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('5.16.0');
         }
 
-        $this->skip('5.16.0', '5.16.3');
+        $this->skip('5.16.0', '5.16.4');
     }
 }
