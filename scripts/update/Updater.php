f502188d07b72aeaa474d7ab746326e27924e367
Aleh Hutnikau <hutnikau@1pt.com>
2017-07-31 10:36:05 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -446,6 +446,6 @@ class Updater extends common_ext_ExtensionUpdater
             }
             $this->setVersion('6.0.0');
         }
-        $this->skip('6.0.0', '6.1.2');
+        $this->skip('6.0.0', '6.1.3');
     }
 }
