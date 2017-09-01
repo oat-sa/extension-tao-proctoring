c453e779ccdcfe3f95af70605ef2c30b88397bd6
Aleksej Tikhanovich <aleksej@taotesting.com>
2017-09-01 11:28:16 +0300

    Bump version

--- manifest.php
+++ manifest.php
@@ -44,7 +44,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '7.1.1',
+    'version' => '7.2.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=10.28.0',
