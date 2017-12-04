a77d214a12e5ab947547b0fc3213acf26a7bed73
Ionut Padurariu <ionut@taotesting.com>
2017-12-04 10:43:09 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -45,7 +45,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '7.12.0',
+    'version' => '7.13.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=14.4.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -548,5 +548,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('7.12.0');
         }
 
+        $this->skip('7.12.0', '7.13.0');
     }
 }
