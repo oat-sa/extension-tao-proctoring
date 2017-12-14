ca1d90c8bb734c8a67946dc2152c8d7e56a544ad
ionutpad <31656944+ionutpad@users.noreply.github.com>
2017-12-14 11:45:35 +0100

    Update Updater.php

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -548,8 +548,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('7.12.0');
         }
         
-        $this->skip('7.12.0', '7.12.1');
-
         $this->skip('7.12.0', '7.13.0');
     }
 }
