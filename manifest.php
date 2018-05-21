098d3c86522288d56ed15c05a7045cde9f021c06
Aleh Hutnikau <hutnikau@1pt.com>
2018-05-21 10:37:31 +0300

    Bump version

--- manifest.php
+++ manifest.php
@@ -45,7 +45,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '8.11.0',
+    'version' => '8.11.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=19.0.0',
