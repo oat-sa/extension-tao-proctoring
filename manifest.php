2630260768b574d2f438e623b66823a184a4e4a8
Ionut Padurariu <ionut@taotesting.com>
2018-08-06 17:17:37 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -46,7 +46,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '10.0.0',
+    'version' => '10.1.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=19.0.0',
