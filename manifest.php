2dd2c12705b24fc9ccf892299dc2e7878f6e9206
Julien Sébire <julien@taotesting.com>
2019-02-28 11:17:04 +0100

    Bump version 12.6.2

--- manifest.php
+++ manifest.php
@@ -47,7 +47,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '12.6.1',
+    'version' => '12.6.2',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao'            => '>=21.8.0',
