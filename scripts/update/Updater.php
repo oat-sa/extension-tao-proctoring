fb43b8cdcc5006a0e1294c529fb77c6290d7ce87
Aleksej Tihanovich <tihanovich.a@gmail.com>
2019-06-07 15:27:38 +0300

    hotfix

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -860,7 +860,7 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('13.1.0');
         }
 
-        $this->skip('13.1.0', '14.0.0.1');
+        $this->skip('13.1.0', '14.0.0.2');
 
     }
 }
