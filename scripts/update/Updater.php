c0eebc58cc53f1d8e8ca9cba99f63f4808011c01
yuryfilipovich <yury.filipovich@1pt.com>
2020-01-03 12:44:06 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -910,6 +910,7 @@ class Updater extends common_ext_ExtensionUpdater
             $this->getServiceManager()->register(GuiSettingsService::SERVICE_ID, $guiService);
             $this->setVersion('17.3.0');
         }
-        $this->skip('17.3.0', '18.2.0');
+
+        $this->skip('17.3.0', '18.2.1');
     }
 }
