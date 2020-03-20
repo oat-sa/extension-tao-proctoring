13d9a161b5f63958a0022248f974a62ad1c201ea
Hanna Dzmitryieva <bziondik@gmail.com>
2020-03-20 18:23:04 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -48,7 +48,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '19.2.1',
+    'version' => '19.3.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao'            => '>=38.9.0',
