a99d6afd4e5048e2719c7d2d56e69429eea800d5
zagovorichev <zagovorichev@gmail.com>
2020-05-18 15:09:09 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -48,7 +48,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '19.8.0',
+    'version' => '19.9.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao'            => '>=41.9.1',
