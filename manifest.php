d087b8aaeab4d8df40d7855678a546d25911fd4b
lecosson <lecosson@gmail.com>
2020-05-18 11:56:30 +0300

    fix unwanted changes

--- manifest.php
+++ manifest.php
@@ -52,7 +52,7 @@ return array(
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao'            => '>=41.9.1',
-        'taoDelivery'    => '>=13.1.2',
+        'taoDelivery'    => '>=14.14.0',
         'taoDeliveryRdf' => '>=7.0.0',
         'taoTestTaker'   => '>=4.0.0',
         'taoQtiTest'     => '>=37.6.1',
