68d19cf31a666385722cb2c99c9e2deed9b71f89
Oleksandr Khitko <oleksandr@taotesting.com>
2020-07-22 11:36:09 +0200

    Version bump

--- manifest.php
+++ manifest.php
@@ -54,7 +54,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '19.18.4',
+    'version' => '19.18.5',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao'            => '>=41.9.1',
