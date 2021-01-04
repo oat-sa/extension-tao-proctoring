dd42867eea87068ef02bec386a5b20cf8e79425e
zagovorichev <zagovorichev@gmail.com>
2021-01-04 14:05:43 +0300

    fix: proctoring: after you perform a function (terminate, add time, refresh page), the checkbox is no longer checked.

--- manifest.php
+++ manifest.php
@@ -54,7 +54,7 @@ return array(
     'label' => 'Proctoring',
     'description' => 'Proctoring for deliveries',
     'license' => 'GPL-2.0',
-    'version' => '19.20.6',
+    'version' => '19.20.7',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao'            => '>=45.6.3',
