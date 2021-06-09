6405a6bb5e0e40d74a9d798f1ee4f1f40d1ca009
Oleksandr Khitko <oleksandr@taotesting.com>
2021-06-09 09:50:50 +0200

    chore: Code style fixes.
    (cherry picked from commit c8481ceaa5a175424b0f4678caf5e9a6b9ad0e8c)

--- migrations/Version202106081338544101_taoProctoring.php
+++ migrations/Version202106081338544101_taoProctoring.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA
+ *
+ */
 
 declare(strict_types=1);
 
@@ -13,12 +31,8 @@ use oat\generis\persistence\PersistenceManager;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
 use oat\taoProctoring\model\monitorCache\implementation\SimpleMonitoringStorage;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version202106081338544101_taoProctoring extends AbstractMigration
 {
-
     public function getDescription(): string
     {
         return 'Creates new `extra_data` column in `delivery_monitoring` to store extra data.';
@@ -37,7 +51,7 @@ final class Version202106081338544101_taoProctoring extends AbstractMigration
                 $table = $schema->getTable(SimpleMonitoringStorage::TABLE_NAME);
                 $table->addColumn(SimpleMonitoringStorage::COLUMN_EXTRA_DATA, Types::JSON, array("notnull" => false));
                 $persistence->getPlatForm()->migrateSchema($fromSchema, $schema);
-            } else if ($platformName == 'postgresql') {
+            } elseif ($platformName == 'postgresql') {
                 $query = sprintf(
                     'ALTER TABLE %s ADD COLUMN %s jsonb',
                     SimpleMonitoringStorage::TABLE_NAME,
@@ -60,7 +74,6 @@ final class Version202106081338544101_taoProctoring extends AbstractMigration
                 )
             );
         }
-
     }
 
     public function down(Schema $schema): void
