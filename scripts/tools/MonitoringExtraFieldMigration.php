48ccbbc6ab4bcdb18550884ab147802f934edf1a
Camille Moyon <camille@taotesting.com>
2021-06-17 17:25:53 +0200

    feat: create script to migrate data from kv to amin table
    (cherry picked from commit 13fa8d33b07188b666c194e233996ea619057372)

--- scripts/tools/MonitoringExtraFieldMigration.php
+++ scripts/tools/MonitoringExtraFieldMigration.php
@@ -15,10 +15,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
- *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
  *
  */
+
+declare(strict_types=1);
 
 namespace oat\taoProctoring\scripts\tools;
 
@@ -26,74 +27,78 @@ use oat\oatbox\extension\script\ScriptAction;
 use common_report_Report as Report;
 use oat\taoProctoring\model\monitorCache\DeliveryMonitoringService;
 use oat\taoProctoring\model\monitorCache\implementation\MonitoringStorage;
+use oat\taoProctoring\model\monitorCache\implementation\SimpleMonitoringStorage;
+use oat\taoProctoring\scripts\install\db\DbSetup;
+use PDO;
 
-
-/**
- * @deprecated
- */
-class KvMonitoringMigration extends ScriptAction
+class MonitoringExtraFieldMigration extends ScriptAction
 {
     protected function provideOptions()
     {
-        return [
-            'fields' => [
-                'prefix' => 'f',
-                'longPrefix' => 'fields',
-                'required' => true,
-                'description' => 'A string contains coma separated list of target fields'
-            ],
-
-            'dataLength' => [
-                'prefix' => 'l',
-                'longPrefix' => 'dataLength',
-                'required' => false,
-                'description' => 'Data length. If present new column(s) will have type VARCHAR($length), otherwise - TEXT column will be created'
-            ],
-
-            'deleteKV' => [
-                'prefix' => 'd',
-                'longPrefix' => 'deleteKV',
-                'defaultValue' => 0,
-                'cast' => 'integer',
-                'required' => false,
-                'description' => 'Indicate whether or not records from KV strorage should be removed'
-            ],
-
-            'strictMode' => [
-                'prefix' => 's',
-                'longPrefix' => 'strictMode',
-                'defaultValue' => 1,
-                'cast' => 'integer',
-                'required' => false,
-                'description' => 'Allow to skip creation of already existing in RDBS columns, should help resuming interrupted migrations '
-            ],
-
-            'persistConfig' => [
-                'flag' => true,
-                'prefix' => 'pc',
-                'longPrefix' => 'persistConfig',
-                'defaultValue' => 1,
-                'description' => 'Indicate whether or not changes in config should be recorded. Please note that applies to the current instance only and changes MUST be distributed across all of them'
-            ],
-
-            'resume' => [
-                'flag' => true,
-                'prefix' => 'r',
-                'longPrefix' => 'resume',
-                'defaultValue' => 1,
-                'description' => 'Indicate whether or not changes processing should be resumed from the last processed frame'
-            ],
-
-            'chunkSize' => [
-                'prefix' => 'c',
-                'defaultValue' => 100,
-                'longPrefix' => 'chunk-size',
-                'cast' => 'integer',
-                'required' => false,
-                'description' => 'Specifies delivery executions amount for processing (chunk size) per iteration for migration'
-            ],
-        ];
+        return [];
     }
+
+//    protected function provideOptions()
+//    {
+//        return [
+//            'fields' => [
+//                'prefix' => 'f',
+//                'longPrefix' => 'fields',
+//                'required' => true,
+//                'description' => 'A string contains coma separated list of target fields'
+//            ],
+//
+//            'dataLength' => [
+//                'prefix' => 'l',
+//                'longPrefix' => 'dataLength',
+//                'required' => false,
+//                'description' => 'Data length. If present new column(s) will have type VARCHAR($length), otherwise - TEXT column will be created'
+//            ],
+//
+//            'deleteKV' => [
+//                'prefix' => 'd',
+//                'longPrefix' => 'deleteKV',
+//                'defaultValue' => 0,
+//                'cast' => 'integer',
+//                'required' => false,
+//                'description' => 'Indicate whether or not records from KV strorage should be removed'
+//            ],
+//
+//            'strictMode' => [
+//                'prefix' => 's',
+//                'longPrefix' => 'strictMode',
+//                'defaultValue' => 1,
+//                'cast' => 'integer',
+//                'required' => false,
+//                'description' => 'Allow to skip creation of already existing in RDBS columns, should help resuming interrupted migrations '
+//            ],
+//
+//            'persistConfig' => [
+//                'flag' => true,
+//                'prefix' => 'pc',
+//                'longPrefix' => 'persistConfig',
+//                'defaultValue' => 1,
+//                'description' => 'Indicate whether or not changes in config should be recorded. Please note that applies to the current instance only and changes MUST be distributed across all of them'
+//            ],
+//
+//            'resume' => [
+//                'flag' => true,
+//                'prefix' => 'r',
+//                'longPrefix' => 'resume',
+//                'defaultValue' => 1,
+//                'description' => 'Indicate whether or not changes processing should be resumed from the last processed frame'
+//            ],
+//
+//            'chunkSize' => [
+//                'prefix' => 'c',
+//                'defaultValue' => 100,
+//                'longPrefix' => 'chunk-size',
+//                'cast' => 'integer',
+//                'required' => false,
+//                'description' => 'Specifies delivery executions amount for processing (chunk size) per iteration for migration'
+//            ],
+//        ];
+//    }
 
     protected function provideDescription()
     {
@@ -111,6 +116,83 @@ class KvMonitoringMigration extends ScriptAction
      */
     protected function run()
     {
+        // check if service is already configured to use JSON column and if column `extra_field` exists
+        $monitoringService = $this->getServiceManager()->get(DeliveryMonitoringService::SERVICE_ID);
+        if (!$monitoringService instanceof SimpleMonitoringStorage) {
+            throw new \Exception('DeliveryMonitoringService is not implementing SimpleMonitoringStorage. Migration aborted');
+        }
+
+        $table = $monitoringService
+            ->getPersistence()
+            ->getDriver()
+            ->getSchemaManager()
+            ->createSchema()
+            ->getTable(SimpleMonitoringStorage::TABLE_NAME);
+        if (!$table->hasColumn(SimpleMonitoringStorage::COLUMN_EXTRA_DATA)) {
+            throw new \Exception(sprintf('Column %s does not exist. Migration aborted', SimpleMonitoringStorage::COLUMN_EXTRA_DATA));
+        }
+
+        //option
+        $chunkSize = 100;
+        $count = $monitoringService->count();
+        $chunk = ceil($count / $chunkSize);
+
+        $removed = $updated = 0;
+
+        echo sprintf('%s delivery executions found', $count) . PHP_EOL;
+
+        for ($i = 0 ; $i < $chunk ; $i++) {
+            $options = [
+                'limit' => $chunkSize,
+                'offset' => 0,
+            ];
+
+            $deliveryExecutions = $monitoringService->find([], $options);
+            echo sprintf('%s delivery executions fetched', count($deliveryExecutions)) . PHP_EOL;
+
+            foreach ($deliveryExecutions as $deliveryExecution) {
+
+                $deliveryExecutionId = $deliveryExecution->get()[DeliveryMonitoringService::DELIVERY_EXECUTION_ID];
+
+                $sql = 'SELECT monitoring_key, monitoring_value FROM kv_delivery_monitoring WHERE parent_id = ?';
+
+                $stmt = $monitoringService->getPersistence()->query($sql, [$deliveryExecutionId]);
+
+                $kvData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                echo sprintf('%s kvData found associated to delivery %s', count($kvData), $deliveryExecutionId) . PHP_EOL;
+
+                foreach ($kvData as $data) {
+                    echo $data['monitoring_key'] . ' => ' . $data['monitoring_value'] . PHP_EOL;
+                    $deliveryExecution->addValue($data['monitoring_key'], $data['monitoring_value']);
+                }
+
+                echo sprintf('Saving delivery %s', $deliveryExecutionId) . PHP_EOL;
+
+                if ($monitoringService->partialSave($deliveryExecution)) {
+                    $updated++;
+                } else {
+                    throw new \Exception(sprintf('Unable to save delivery execution %s', $deliveryExecutionId));
+                }
+
+//                $sql = 'DELETE FROM kv_delivery_monitoring WHERE parent_id = ?';
+//
+//                $removed += $monitoringService->getPersistence()->exec($sql, [$deliveryExecutionId]);
+            }
+        }
+
+        echo sprintf('Saved delivery execution: %s', $updated) . PHP_EOL;
+        echo sprintf('ExtraData removed from KV table: %s', $removed) . PHP_EOL;
+
+        die;
+        // foreach count of DX on KV table
+            // fetch chunk of X data by delivery execution
+            // foreach X dx,
+                // json encode ? or set as part DeliveryMonitoringData
+                // put in buffer to later update
+            // update delivery_monitoring
+            // delete kv_delivery_monitoring
+
         $subReport = new Report(Report::TYPE_INFO);
         $removeKV = $this->getOption('deleteKV');
         $persistConfig = $this->getOption('persistConfig');
