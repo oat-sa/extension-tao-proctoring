29bfcccbd0e6f79541e5899c5262348b0766b5e3
Yohan Laborda <yohan.laborda@taotesting.com>
2021-12-23 12:36:39 +0100

    test: add new tests for TestRunnerMessageService

---
+++ test/unit/model/implementation/TestRunnerMessageServiceTest.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoProctoring\test\unit\model\implementation;
+
+use oat\oatbox\service\ServiceManager;
+use oat\tao\model\featureFlag\FeatureFlagCheckerInterface;
+use oat\taoProctoring\model\implementation\TestRunnerMessageService;
+use qtism\runtime\tests\AssessmentTestSession;
+use PHPUnit\Framework\TestCase;
+
+final class TestRunnerMessageServiceTest extends TestCase
+{
+    /**
+     * @var TestRunnerMessageService
+     */
+    private $testRunnerMessageService;
+
+    /**
+     * @var ServiceManager
+     */
+    private $serviceManager;
+
+    /**
+     * @var FeatureFlagCheckerInterface
+     */
+    private $featureFlagChecker;
+
+    /**
+     * @var AssessmentTestSession
+     */
+    private $assessmentTestSession;
+
+    protected function setUp(): void
+    {
+        $this->testRunnerMessageService = new TestRunnerMessageService();
+        $this->featureFlagChecker = $this->createMock(FeatureFlagCheckerInterface::class);
+        $this->serviceManager = $this->createMock(ServiceManager::class);
+        $this->serviceManager->method('get')->willReturn($this->featureFlagChecker);
+        $this->assessmentTestSession = $this->createMock(AssessmentTestSession::class);
+    }
+
+    /**
+     * @dataProvider providerProctorRoles
+     */
+    public function testGetPausedStateMessage(array $proctorRoles, string $message): void
+    {
+        $testRunnerMessageService = $this->getClassTestRunnerMessageService($this->serviceManager, ['proctorRoles' => $proctorRoles]);
+        $pausedStateMessage = $testRunnerMessageService->getPausedStateMessageFrom($this->assessmentTestSession);
+
+        self::assertSame($message, $pausedStateMessage);
+    }
+
+    public function testGetPausedStateMessageReturnEmpty(): void
+    {
+        $this->featureFlagChecker->method('isEnabled')->willReturn(true);
+        $testRunnerMessageService = $this->getClassTestRunnerMessageService($this->serviceManager);
+        $pausedStateMessage = $testRunnerMessageService->getPausedStateMessageFrom($this->assessmentTestSession);
+
+        self::assertEmpty($pausedStateMessage);
+    }
+
+    public function providerProctorRoles(): array
+    {
+        return [
+            'message with defined roles' => [
+                (new \common_session_AnonymousSession())->getUserRoles(),
+                'The assessment has been suspended. To resume your assessment, please relaunch it and contact your proctor if required.'
+            ],
+            'message without defined roles' => [
+                [],
+                'The assessment has been suspended. To resume your assessment, please relaunch it.'
+            ]
+        ];
+    }
+
+    /**
+     * @param array $options
+     * @param ServiceManager $serviceManager
+     * @return TestRunnerMessageService
+     */
+    private function getClassTestRunnerMessageService($serviceManager, $options = []): TestRunnerMessageService
+    {
+        return new class ($options, $serviceManager) extends TestRunnerMessageService {
+            /**
+             * @var ServiceManager
+             */
+            private $serviceManager;
+
+            public function __construct($options = [], $serviceManager)
+            {
+                parent::__construct($options);
+                $this->serviceManager = $serviceManager;
+            }
+
+            /**
+             * @param AssessmentTestSession $testSession
+             * @return string
+             */
+            public function getPausedStateMessageFrom(AssessmentTestSession $testSession): string
+            {
+                return parent::getPausedStateMessage($testSession);
+            }
+
+            /**
+             * @return ServiceManager
+             */
+            public function getServiceLocator(): ServiceManager
+            {
+                return $this->serviceManager;
+            }
+        };
+    }
+}
