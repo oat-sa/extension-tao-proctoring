0b3dd7055a5bf998470ff79a252b8dce14a51a88
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2021-12-24 14:35:43 +0100

    chore: remove auto-generated doc block from `Version202112231653214101_taoProctoring` migration
    Co-authored-by: Mikhail Kamarouski <4569734+boajer@users.noreply.github.com>

--- migrations/Version202112231653214101_taoProctoring.php
+++ migrations/Version202112231653214101_taoProctoring.php
@@ -8,9 +8,6 @@ use Doctrine\DBAL\Schema\Schema;
 use oat\tao\scripts\SyncModels;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version202112231653214101_taoProctoring extends AbstractMigration
 {
     public function getDescription(): string
